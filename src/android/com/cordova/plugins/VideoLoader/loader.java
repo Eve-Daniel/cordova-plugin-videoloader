20a1b0611a0a53202a587ed579f57532fa2d130b
eve <eve@ironstar.pw>
2017-10-08 02:28:36 +0300

    aaa

--- src/android/com/cordova/plugins/VideoLoader/loader.java
+++ src/android/com/cordova/plugins/VideoLoader/loader.java
@@ -82,10 +82,10 @@ public class loader implements Runnable {
     @Override
     public void run() {
         this.onSuccess("aaaa");
-        this.onSuccess(this.appcontext.getDir($DIR,Context.MODE_PRIVATE).getAbsolutePath());
-        this.onFail(this.appcontext.getDir($DIR,Context.MODE_PRIVATE).getAbsolutePath());
+        //this.onSuccess(this.appcontext.getDir($DIR,Context.MODE_PRIVATE).getAbsolutePath());
+        //this.onFail(this.appcontext.getDir($DIR,Context.MODE_PRIVATE).getAbsolutePath());
         this.onSuccess("bbbb");
-        File $dir = this.appcontext.getDir($DIR,Context.MODE_PRIVATE); //new File($DIR);
+        File $dir = new File($DIR);
         if (!$dir.exists()) {
             $dir.mkdirs();
         }
